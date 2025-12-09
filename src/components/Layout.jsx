ce3012ec8795efad07d2970e21ca426e3d1b5f97
Diego Castillo <diegocastillo2631@gmail.com>
2025-12-09 14:48:21 -0500

    first commit

---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+// Aquí importan el Header y Footer, haganlos por separado como componentes
+
+import Header from "./header.jsx"
+
+export default function Layout({ children }) {
+  return (
+    <main className="mx-auto max-w-3xl p-4 sm:p-6 font-sans leading-relaxed text-gray-800 dark:text-gray-100 bg-white dark:bg-neutral-900 min-h-screen">
+    <Header/>
+
+      <section>{children}</section>
+
+      {/*Aquí colocan el Footer*/}
+    </main>
+  );
+}
